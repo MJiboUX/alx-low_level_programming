cf4a881b6510742a8fd4ef55c1570bf5ae0cb700
Mubarak Mohammad Jibo <mubarrakmohammadjibo@gmail.com>
2023-08-06 12:51:31 +0100

    Update 100-get_endianness.c

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,12 +1,18 @@
+#include <stdio.h>
 #include "main.h"
-/**
- * get_endianness - return the endianness of the machine
- *
- * Return: 0 for big endian, 1 for little endian
- */
-int get_endianness(void)
-{
-	int n = 1;
 
-	return (*((char *) &n) + '0');
+int main(void)
+{
+    int n;
+
+    n = get_endianness();
+    if (n != 0)
+    {
+        printf("Little Endian\n");
+    }
+    else
+    {
+        printf("Big Endian\n");
+    }
+    return (0);
 }
